4c6dd06a48fcdba02be276110cbcafa933766bdc
Henry-E <Henry-E@users.noreply.github.com>
2016-01-25 18:43:24 +0000

    not really a change

--- build/snake_visualisation.js
+++ build/snake_visualisation.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	var food = [{x: 0, y:0}];
   // var food = [{x:4, y:0}, {x:3, y:1}, {x:2, y:2}, {x:1, y:1}, {x:0, y:0}, {x:1, y:3}, {x:0, y:4}, {x:3, y:3}, {x:4, y:4}];
 	var N = 5;
-	var num_agents = Math.round(w * h) / 3000;
+	var num_agents = Math.round(w * h / 3300);
 
 
 	// I haven't really implemented this bit yet, it would
